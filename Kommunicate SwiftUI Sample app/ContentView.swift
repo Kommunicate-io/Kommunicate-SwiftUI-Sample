4e86779cc6c8fb2599451ab6bf0985aa87855cc5
mukeshthawani <mukesh9039@gmail.com>
2020-09-18 17:36:00 +0530

    Initial commit

---
+++ Kommunicate SwiftUI Sample app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kommunicate SwiftUI Sample app
+//
+//  Created by Mukesh on 18/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kommunicate SwiftUI Sample appTests/Kommunicate_SwiftUI_Sample_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  Kommunicate_SwiftUI_Sample_appTests.swift
+//  Kommunicate SwiftUI Sample appTests
+//
+//  Created by Mukesh on 18/09/20.
+//
+
+import XCTest
+@testable import Kommunicate_SwiftUI_Sample_app
+
+class Kommunicate_SwiftUI_Sample_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
